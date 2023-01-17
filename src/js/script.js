632a117559a7911e2a0f40a4e125533786fde378
AlexFlorenco <aflorenco13@gmail.com>
2023-01-17 09:56:34 -0300

    feat:Conversão direta selecionando moedas

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,9 @@
 function mostrarCotacao(result) {
-    let cotacao = document.getElementById('resultado');
+    let cotacao = document.getElementById('conversao');
 
     for (const chave in result) {
-        if (chave === 'info') {
-            let textElement = document.createElement("h2");
-            let textNode = document.createTextNode(`Cotação atual: ${result[chave].rate} ${result['query'].from}`);
-            cotacao.appendChild(textElement.appendChild(textNode))
+        if (chave === 'result') {
+            cotacao.innerHTML = `${result[chave]} ${result['query'].to}`;
         }
 
         if (chave === 'result') {
@@ -15,12 +13,12 @@ function mostrarCotacao(result) {
     }
 }
 
-const btn = document.getElementById('button').addEventListener('click', () => {
+const btn = document.getElementById('converter').addEventListener('click', () => {
     var myHeaders = new Headers();
     myHeaders.append("apikey", "NR44ed5npzuK0M2E7Jowhbs8gQOk2yjD");
     const access_key = 'NR44ed5npzuK0M2E7Jowhbs8gQOk2yjD';
-    const from = document.getElementById('moeda').value;
-    const to = 'BRL';
+    const from = document.getElementById('moeda1').value;
+    const to = document.getElementById('moeda2').value;
     let amount = document.getElementById('valor').value;
 
     fetch(`https://api.apilayer.com/fixer/convert?to=${to}&from=${from}&amount=${amount}`, {
